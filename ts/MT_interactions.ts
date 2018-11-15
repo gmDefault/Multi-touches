72f7346c4444b1686c4f58514a843fc0b080ed5d
bastien-7 <bastien.terrier7@gmail.com>
2018-11-15 14:50:01 +0100

    Adding comments to explain our implementation

--- ts/MT_interactions.ts
+++ ts/MT_interactions.ts
@@ -25,6 +25,7 @@ function multiTouch(element: HTMLElement) : void {
                 useCapture: false,
                 action: (evt : TouchEvent) : boolean => {
                     console.log("entering Translating");
+                    // when a first finger press the screen, we store its position in Pt1
                     Pt1_coord_parent = transfo.getPoint(evt.changedTouches.item(0).pageX,evt.changedTouches.item(0).pageY );
                     originalMatrix = transfo.getMatrixFromElement(element);
                     Pt1_coord_element = Pt1_coord_parent.matrixTransform(originalMatrix.inverse());
@@ -39,7 +40,9 @@ function multiTouch(element: HTMLElement) : void {
                     console.log("DRAGGING");
                     evt.preventDefault();
                     evt.stopPropagation();
+                    //save the new position of the finger
                     Pt1_coord_parent = transfo.getPoint(evt.changedTouches.item(0).pageX,evt.changedTouches.item(0).pageY );
+                    //apply the transformation : translation
                     transfo.drag(element,originalMatrix,Pt1_coord_element,Pt1_coord_parent);
                     return true;
                 }
@@ -51,6 +54,7 @@ function multiTouch(element: HTMLElement) : void {
                 useCapture: true,
                 action: (evt : TouchEvent) : boolean => {
                     console.log("INCATIVE");
+                    // we could have set Pt1 and Pt2 to null, but it is not necessary.
                     return true;
                 }
             },
@@ -60,6 +64,7 @@ function multiTouch(element: HTMLElement) : void {
                 useCapture: true,
                 action: (evt : TouchEvent) : boolean => {
                     console.log("entering Rotozooming");
+                    // when the other finger press the screen, we save its position and strat rotozooming
                     originalMatrix = transfo.getMatrixFromElement(element);
                     Pt2_coord_parent= transfo.getPoint(evt.changedTouches.item(0).pageX,evt.changedTouches.item(0).pageY );
                     Pt2_coord_element = Pt2_coord_parent.matrixTransform(originalMatrix.inverse());
@@ -74,15 +79,18 @@ function multiTouch(element: HTMLElement) : void {
                     evt.preventDefault();
                     evt.stopPropagation();
                     console.log("Rotozooming");
+                    //if both points are moving
                     if(evt.changedTouches.length===2){
                         Pt2_coord_parent = transfo.getPoint(evt.changedTouches.item(1).pageX,evt.changedTouches.item(1).pageY );
                         Pt1_coord_parent = transfo.getPoint(evt.changedTouches.item(0).pageX,evt.changedTouches.item(0).pageY );
                     }
+                    //if just one point is moving
                     else if(evt.changedTouches.length===1){
                         if(evt.changedTouches.item(0).identifier!==0){
                             Pt2_coord_parent= transfo.getPoint(evt.changedTouches.item(0).pageX,evt.changedTouches.item(0).pageY );
                         }
                     }
+                    //apply the transformation (rotate, drag, zoom)
                     transfo.rotozoom(element,originalMatrix,Pt1_coord_element,Pt1_coord_parent,Pt2_coord_element,Pt2_coord_parent);
                     return true;
                 }
@@ -95,6 +103,8 @@ function multiTouch(element: HTMLElement) : void {
                 action: (evt : TouchEvent) : boolean => {
                     const touch = getRelevantDataFromEvent(evt);
                     console.log("entering Translating");
+                    // if a finger is released, we will continue and dragging with the other finger ! 
+                    // we have to set the first point (Pt1) with the coord of the remaining finger
                     if(evt.changedTouches.item(0).identifier===0){
                         Pt1_coord_parent=Pt2_coord_parent;
                         Pt1_coord_element=Pt2_coord_element;
